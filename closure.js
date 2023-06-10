6cf12e15eea15e36ed792d8170b26c19edaaff67
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-10 23:33:20 +0300

    Create closure.js

---
+++ closure.js
@@ -0,0 +1,10 @@
+// // Lexical Scope defines how variable names are resolved in nested functions.
+// // Nested (child) functions have access to the scope of their parent functions.
+// // This is often confused with closure, but lexical scope is only an important part of closure.
+
+// // w3schools: "A closure is a function having access to the parent scope, even after the parent function has closed (returned)."
+
+// // A closure is created when we define a function, not when a function is executed.
+
+// // global scope
+// let x = 1;
