cf0db7001316c2dbadf70fd5df65fcd333ac1eb4
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-10 23:38:56 +0300

    Add real-world examples

--- recursion.js
+++ recursion.js
@@ -87,3 +87,64 @@ console.log(fibonacciPos(8));
 //   if (pos < 2) return pos;
 //   return fibPos(pos - 1) + fibPos(pos - 2);
 // }
+
+// What we wanna ask ourselves. Is this more readable? Is this elegant code? 
+const fibPos = pos => pos < 2 ? pos : fibPos(pos - 1) + fibPos(pos - 2);
+
+console.log(fibPos(8));
+
+// Real-Life Examples:
+// 1) Continuation Token from an API
+
+const getAWSProductIdImages = async () => {
+  // get the data with await fetch request
+
+  if (data.IsTruncated) {
+    // recursive
+    return await getAWSProductIdImages(
+      productId,
+      s3, // connection to s3
+      resultArray, // Accumulator
+      data.NextContinuationToken
+    );
+  }
+
+  return resultArray;
+}
+
+// 2) A Parser:
+// a company directory
+// a file directory
+// the DOM - a web crawler
+// An XML or JSON data export
+
+// Export from your streaming service like Spotify, YT Music, etc.
+const artistByGenre = {
+  jazz: ["Miles Davis", "John Coltrane"],
+  rock: {
+    classic: ["Bob Seger", "The Eagles"],
+    hair: ["Def Leppard", "Whitesnake", "Poison"],
+    alt: {
+      classic: ["Pearl Jam", "The Killers"],
+      current: ["Joywave", "Sir Sly"]
+    }
+  },
+  unclassified: {
+    new: ["Caamp", "Neil Young"],
+    classic: ["Seal", "Morcheeba", "Chris Stapleton"]
+  }
+}
+
+const getArtistNames = (dataObj, arr = []) => {
+  Object.keys(dataObj).forEach(key => {
+    if (Array.isArray(dataObj[key])) {
+      return dataObj[key].forEach(artist => {
+        arr.push(artist);
+      });
+    }
+    getArtistNames(dataObj[key], arr);
+  });
+  return arr;
+}
+
+console.log(getArtistNames(artistByGenre));
