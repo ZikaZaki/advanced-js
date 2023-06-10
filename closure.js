ab229232d45d0a53809ade98b43fd7f27ccbd5a1
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-10 23:33:55 +0300

    Update closure.js

--- closure.js
+++ closure.js
@@ -8,3 +8,28 @@
 
 // // global scope
 // let x = 1;
+
+// const parentFunction = () => {
+//   // local scope
+//   let myValue = 2;
+//   console.log(x);
+//   console.log(myValue);
+
+//   // Even though, we can say that childFunction has a closure over the parentFunction scope and a closure over the global scope, but it's not directely giving an example to closures. It's just a lexical scope.
+//   const childFunction = () => {
+//     console.log(x += 5);
+//     console.log(myValue += 1);
+//   }
+
+//     console.log("now we are having a real example of closure, because the child function will be executed after the parent function closed (returned).");
+
+//   return childFunction;
+// }
+
+// // parentFunction();
+
+// const childFun = parentFunction();
+// childFun();
+// childFun();
+// const childFun2 = parentFunction();
+// childFun2();
