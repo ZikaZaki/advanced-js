8e1adc5cce02c02a2e9493cabb2c1ec4b5675e04
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-10 23:35:22 +0300

    Update __proto__.js

--- __proto__.js
+++ __proto__.js
@@ -1,1 +1,17 @@
+// Prototypal Inheritance and the Prototype Chain.
 
+// ES6 introduced classes which is the modern way to construct objects.
+
+// That said, prototypal inheritance and the prototype chain are:
+// 1) "under the hood", (ES6 Classes are considered "syntactic sugar")
+// 2) often in interview questions.
+// 3) and are useful to understand.
+
+// Object literals
+const person = {
+  alive: true
+}
+
+const musician = {
+  plays: true
+}
