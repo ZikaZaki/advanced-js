ed5f9dbecbc8379f1aaba6003958d4b18bb9bb52
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-10 23:37:15 +0300

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -1,1 +1,18 @@
+// Official Definition of Recursion:
+// In computer science, recursion is a method of solving a problem where the solution depends on solutions to smaller instances of the same problem.
 
+// Unofficial Definition of Recursion:
+// "Any situation where you do something, and depending on the results, you might do it again."
+
+// In programming, recursion occurs when a function calls itself.
+// Any iterator function (aka function with a loop) can be recursive instead.
+
+// iterator function
+const countToTen = (num = 1) => {
+  while (num <= 10) {
+    console.log(num);
+    num++;
+  }
+}
+
+// countToTen();
