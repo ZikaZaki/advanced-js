a4d1cb6b7a3484fa41f2a1e8c948df3b72f45aec
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-10 23:38:17 +0300

    Update recursion.js with more functions

--- recursion.js
+++ recursion.js
@@ -16,3 +16,74 @@ const countToTen = (num = 1) => {
 }
 
 // countToTen();
+
+// recursive functions have 2 parts:
+// 1) the recursive call to the function
+// 2) at least one condition to exit
+
+const recurToTen = (num = 1) => {
+  if (num > 10) return;
+
+  console.log(num);
+  num++;
+  recurToTen(num);
+}
+
+recurToTen();
+
+// "With Great Power Comes Great Responsibility"
+// Reasons to use (not abuse) Recursion
+// 1) Less Code
+// 2) Elegant Code (aka Pleasing to Look at)
+// 3) Increased Readability
+
+// Reasons to NOT use Recursion
+// 1) Performance
+// 2) Possibly more difficult to debug (aka follow the logic)
+// 3) Is the Readability Improved?
+
+// The Standard Example: The Fibonacci Sequence
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, etc.
+
+// Without Recursion:
+const fibonacci = (num, array = [0, 1]) => {
+  while (num > 2) {
+    // const [nextToLast, last] = array.slice(array.length -2);
+    const [nextToLast, last] = array.slice(-2);
+    array.push(nextToLast + last);
+    num -= 1;
+  }
+  return array;
+}
+
+console.log(fibonacci(12));
+
+// With Recursion:
+const fib = (num, array = [0, 1]) => {
+  if (num <= 2) return array;
+  const [nextToLast, last] = array.slice(-2);
+  return fib(num - 1, [...array, nextToLast + last]);
+}
+
+console.log(fib(12));
+
+// What number is in the nth position of the Fibonacci Sequence?
+
+// Without Recursion:
+const fibonacciPos = (pos) => {
+  if (pos <= 1) return pos;
+  const seq = [0, 1];
+  for (let i = 2; i <= pos; i++) {
+    const [nextToLast, last] = seq.slice(-2);
+    seq.push(nextToLast + last);
+  }
+  return seq[pos];
+}
+
+console.log(fibonacciPos(8));
+
+// With Recursion:
+// const fibPos = (pos) => {
+//   if (pos < 2) return pos;
+//   return fibPos(pos - 1) + fibPos(pos - 2);
+// }
