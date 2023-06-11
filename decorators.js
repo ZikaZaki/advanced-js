fbc3ebe985da069101d0f888a9a70c3992754d2f
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-11 23:59:45 +0300

    Add sum function

--- decorators.js
+++ decorators.js
@@ -1,1 +1,17 @@
+// JavaScript Decorator Functions
 
+// Decorators wrap a function in another function.
+
+// These wrappers "decorate" the original function
+// with new capabilities, without changing its strcture.
+
+// The concept of decoration is not exclusive to JavaScript
+
+// Benefits: D.R.Y. and clean code through composition
+
+// Example 1:
+// Using closure to log new many times a function is called
+
+let sum = (...args) => {
+  return [...args].reduce((acc, num) => acc + num);
+}
