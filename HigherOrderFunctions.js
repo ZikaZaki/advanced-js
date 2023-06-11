e724ff4928e47075f2e9d38703953f63ea0eab29
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-11 23:47:06 +0300

    Add forEach() HOF

--- HigherOrderFunctions.js
+++ HigherOrderFunctions.js
@@ -1,1 +1,15 @@
+// Higher Order Functions
 
+// A higher order function is a function that does at least one
+// of the followings:
+// ** Takes one or more functions as an argument (parameter)
+// ** Returns a function as its result
+
+// forEach()
+import { posts } from "./posts.js";
+
+posts.forEach(post => {
+  console.log(post);
+});
+
+console.clear();
