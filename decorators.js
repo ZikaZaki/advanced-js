54b41328708846f4d15d4ebfb28c758aa13ca110
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-12 00:34:47 +0300

    Add 1st example of decorators

--- decorators.js
+++ decorators.js
@@ -10,8 +10,27 @@
 // Benefits: D.R.Y. and clean code through composition
 
 // Example 1:
-// Using closure to log new many times a function is called
+// Using closure to log how many times a function is called
 
+// Core Function
 let sum = (...args) => {
   return [...args].reduce((acc, num) => acc + num);
 }
+
+// Decorator Function
+const callCounter = (fn) => {
+  let count = 0;
+
+  return (...args) => {
+    // write to logs, console, db, etc
+    console.log(`sum has been called ${count += 1} times`);
+    return fn(...args);
+  }
+}
+
+// This why we used "let" to define the sum() function,
+// so that we can reassign it with a decorator
+sum = callCounter(sum);
+console.log(sum(2, 3, 5));
+console.log(sum(1, 5));
+console.log(sum(14, 5));
