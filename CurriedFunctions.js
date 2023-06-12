369a4a402fe8ad0b8e30e8e62d0a5d6ce1bf0a42
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-13 02:12:13 +0300

    Add 1st example

--- CurriedFunctions.js
+++ CurriedFunctions.js
@@ -1,1 +1,16 @@
+// Currying
+// Named after Haskell B. Curry
+// Concept from lambda calculus
+// Currying takes a function that receives more than one parameter and breaks it into a series of unary (one parameter) functions. Therefore, a curried function only takes one parameter at a time.
 
+// Currying can look like this:
+const buildSandwitch = (ingredient1) => {
+  return (ingredient2) => {
+    return (ingredient3) => {
+      return `${ingredient1}, ${ingredient2}, ${ingredient3}`;
+    }
+  }
+}
+
+const mySandwitch = buildSandwitch("Bacon")("Lettuce")("Tomato");
+console.log(mySandwitch);
