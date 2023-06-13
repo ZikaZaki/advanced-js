eb547213a2f07b23488608af25b406b3f2773a23
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-13 23:35:01 +0300

    Add comment for primitive

--- Shallow_vs_Deep_Copy.js
+++ Shallow_vs_Deep_Copy.js
@@ -1,1 +1,14 @@
+// Foundational knowledge for writing Pure Functions
 
+// JavaScrpt Data Types
+
+// Primitive vs Structural
+
+/* Primitive:
+  1) undefined
+  2) Boolean
+  3) Number
+  4) String
+  5) BigInt
+  6) Symbol
+*/
