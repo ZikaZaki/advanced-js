c1c5270949c668d4262b2715cfedfc98d7834c6f
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-14 01:47:51 +0300

    Add benefits comment of PureFunctions

--- PureFunctions.js
+++ PureFunctions.js
@@ -1,1 +1,13 @@
+// Pure Functions
+
+// A part of the Functional Programming Paradigm
+
+/* Why write Pure Functions?
+  1) Clean Code
+  2) Easy to Test
+  3) Easy to Debug
+  4) Decoupled and Independant (That's they're not married to the rest of your application).
+  5) Could be added to your utility functions.
+*/
+
 
