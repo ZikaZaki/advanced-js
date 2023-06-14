14bdff283fa1203d3573c6910dfc478d98d12d5c
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-15 01:50:04 +0300

    Add example of IIFE with the function keyword

--- IIFE.js
+++ IIFE.js
@@ -6,3 +6,15 @@
 (() => {
   // do stuff
 })(); 
+
+// With the function keyword:
+(function () {
+  // do stuff
+})();
+
+// With a function name (allows for recursion);
+(function myIIFE(num = 0) {
+  num++;
+  console.log(num);
+  return num !== 5 ? myIIFE(num) : console.log('finished!');
+})();
