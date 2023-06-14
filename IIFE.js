92f23155088acbff48eefc49627dd34ce2880fd7
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-15 01:49:14 +0300

    Add anonymous arrow function variation of IIEF

--- IIFE.js
+++ IIFE.js
@@ -1,1 +1,8 @@
+// IIFE - Immediately-Invoked Function Expression
+// Pronounced "Iffy" by Ben Alman who introudced the acronym.
 
+// Variations:
+// With anonymous arrow function inside:
+(() => {
+  // do stuff
+})(); 
