1d3ad240fcffb9fc864aedab6f9752625a7c39b4
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-15 01:50:38 +0300

    Add 1st reason of using IIFE with example

--- IIFE.js
+++ IIFE.js
@@ -18,3 +18,21 @@
   console.log(num);
   return num !== 5 ? myIIFE(num) : console.log('finished!');
 })();
+
+/* Reasons for using IIFE: */
+// Reason (1) Does not pollute the golbal object namespace.
+// -----------------------------------------------
+// Example with polluted golbal object namespace:
+// -----------------------------------------------
+   const x = "whatever";
+   const helloWorld = () => "Hello World!";
+// --------------------------------------------------
+// Example without polluting golbal object namespace:
+// --------------------------------------------------
+// Isolate declarations within the function
+   (() => {
+       const x = "IIFE whatever";
+       const helloWorld = () => "Hello IIFE!";
+       console.log(x);
+       console.log(helloWorld());
+   })();
