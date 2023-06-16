6c1fd361b95deb9a2085b4c3c944f9bca900e3e1
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-16 22:10:29 +0300

    Add Initializing example one

--- hoisting.js
+++ hoisting.js
@@ -26,3 +26,12 @@ x = 5; // Assign 5 to x
 
 elem = document.getElementById("demo"); // Find an element
 elem.innerHTML = x;                     // Display x in the element
+
+// JavaScript Initializations are Not Hoisted
+// JavaScript only hoists declarations, not initializations.
+// Example 1
+var x = 5; // Initialize x
+var y = 7; // Initialize y
+
+elem = document.getElementById("demo"); // Find an element
+elem.innerHTML = x + " " + y;           // Display x and y
