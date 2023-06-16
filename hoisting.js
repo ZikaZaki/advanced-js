e83561b7c7ef4aa3cd0eb174b44bbe6c608f947c
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-16 22:10:59 +0300

    Add initializing example two

--- hoisting.js
+++ hoisting.js
@@ -33,5 +33,13 @@ elem.innerHTML = x;                     // Display x in the element
 var x = 5; // Initialize x
 var y = 7; // Initialize y
 
+// Example 2
+var x = 5; // Initialize x
+
+elem = document.getElementById("demo"); // Find an element
+elem.innerHTML = x + " " + y;           // Display x and y
+
+var y = 7; // Initialize y
+
 elem = document.getElementById("demo"); // Find an element
 elem.innerHTML = x + " " + y;           // Display x and y
