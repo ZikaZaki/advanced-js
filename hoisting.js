5ff0af9e4fbec2f894c43ce10f2d2cc4e7f700f4
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-16 22:09:02 +0300

    Add example two

--- hoisting.js
+++ hoisting.js
@@ -19,3 +19,10 @@ elem = document.getElementById("demo"); // Find an element
 elem.innerHTML = x;                     // Display x in the element
 
 var x; // Declare x
+
+// Add example two
+var x; // Declare x
+x = 5; // Assign 5 to x
+
+elem = document.getElementById("demo"); // Find an element
+elem.innerHTML = x;                     // Display x in the element
