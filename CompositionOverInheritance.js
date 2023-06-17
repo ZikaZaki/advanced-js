b154f1777c6dcab4ce71c12f8dd3b6afdeb2f698
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-17 21:58:25 +0300

    Add salad class

--- CompositionOverInheritance.js
+++ CompositionOverInheritance.js
@@ -10,3 +10,14 @@ class Pizza {
   bake() { console.log("Baking..."); }
   ready() { console.log("Ready!"); }
 }
+
+// Problem: Repeating methods - Not D.R.Y.
+class Salad {
+  constructor(size, dressing) {
+    this.size = size;
+    this.dressing = dressing;
+  }
+  prepare() { console.log("Preparing..."); }
+  toss() { console.log("Tossing..."); }
+  ready() { console.log("Ready!"); }
+}
