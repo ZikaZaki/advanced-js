08a4f864224f8929072f9772d002e68a4689c3f8
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-17 21:56:10 +0300

    Add pizza class

--- CompositionOverInheritance.js
+++ CompositionOverInheritance.js
@@ -1,1 +1,12 @@
-
+/* Inheritance */
+class Pizza {
+  constructor(size, crust, sauce) {
+    this.size = size;
+    this.crust = crust;
+    this.sauce = sauce;
+    this.toppings = [];
+  }
+  prepare() { console.log("Preparing..."); }
+  bake() { console.log("Baking..."); }
+  ready() { console.log("Ready!"); }
+}
