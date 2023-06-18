b2c644f302263ad931aefed7751b7e7a73f4e69b
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-18 22:58:47 +0300

    Add createPizza function

--- CompositionOverInheritance.js
+++ CompositionOverInheritance.js
@@ -77,3 +77,19 @@ const butter = () => {
     butter: () => console.log("Buttering the crust...")
   }
 }
+
+const createPizza = (size, crust, sauce) => {
+  const pizza = {
+    size: size,
+    crust: crust,
+    sauce: sauce,
+    toppings: []
+  }
+
+  return {
+    ...pizza,
+    ...prepare(),
+    ...bake(),
+    ...ready()
+  }
+}
