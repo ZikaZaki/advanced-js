228bd71988e3cbca7d4f06121a88d6ac9930a9b4
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-30 22:39:20 +0300

    Add examples of normal functions

--- pipe_compose_functions.js
+++ pipe_compose_functions.js
@@ -1,1 +1,16 @@
+/*
+* Functional Programming: often uses 'pipe' and 'compose' = higher order functions (HOC).
+* A Higher Order Function (HOC) is any function which takes a function as an argument, returns a function, or both.
+*/
 
+/* Here's how a "compose" function works:
+   Starting with small unary (one parameter) functions
+*/
+const add2 = (x) => x + 2;
+const subtract1 = (x) => x - 1;
+const multiplyBy5 = (x) => x * 5;
+
+// Notice how the functions execute from inside to outside & right to left.
+const result = multiplyBy5(subtract1(add2(4)));
+console.log(result);
+// The above is NOT a compose function - let's make one.
