5b0f14425c2fd6a25a5b1aa1c74b2070b99a39b7
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-06-30 22:39:49 +0300

    Add 1st example of compose func

--- pipe_compose_functions.js
+++ pipe_compose_functions.js
@@ -14,3 +14,17 @@ const multiplyBy5 = (x) => x * 5;
 const result = multiplyBy5(subtract1(add2(4)));
 console.log(result);
 // The above is NOT a compose function - let's make one.
+
+/*
+* Making our own compose and pipe functions
+* NOTE: Ramda.js and lodash libraries have their own built-in compose and pipe functions. lodash calls pipe "flow".
+* The higher order function "reduce" takes a list of values and applies a function to each of those values, accumulating a single result.
+*/
+
+/*
+* To get the compose order from right to left as we see with nested function calls in our example above, we need reduceRight...
+*/
+const compose = (...fns) => val => fns.reduceRight((prev, fn) => fn(prev), val);
+
+const compResult = compose(multiplyBy5, subtract1, add2)(4);
+console.log("Compose Result: ", compResult);
