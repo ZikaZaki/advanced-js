5426fde01470d75e866a6061509ac4f101035875
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-01 23:02:38 +0300

    Add switch stmt

--- stop_using_switch.js
+++ stop_using_switch.js
@@ -1,1 +1,25 @@
-
+const extension = '/';
+  // Switch statement
+let contentType;
+switch(extension) {
+  case '.css':
+    contentType = 'text/css';
+    break;
+  case '.js':
+    contentType = 'text/javascript';
+    break;
+  case '.json':
+    contentType = 'application/json';
+    break;
+  case '.jpg':
+    contentType = 'image/jpeg';
+    break;
+  case '.png':
+    contentType = 'image/png';
+    break;
+  case '.txt':
+    contentType = 'text/plain';
+    break;
+  default:
+    contentType = 'text/html';
+}
