b1cc44ce0c0b2b702afb01e2718c4eeee61ebc24
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-02 00:02:50 +0300

    Add 2nd example

--- checking_arrays_correctly.js
+++ checking_arrays_correctly.js
@@ -4,3 +4,11 @@ let myArray = [];
 
 // Array have a length property
 console.log(myArray.length);
+
+// Mistake I've made too often!
+console.log(myArray.length ? true : false);
+
+// Here's why:
+myArray = undefined; // same if null
+// Error:
+// console.log(myArray.length ? true : false);
