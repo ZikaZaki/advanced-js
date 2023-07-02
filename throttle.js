85643bb32a75cc0cb0df4434915758de1d6dd633
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-03 01:27:17 +0300

    Add summary comments

--- throttle.js
+++ throttle.js
@@ -1,3 +1,18 @@
+/* ================================================================Summary================================================================
+* When to use Debounce?----------------
+* Debounce is used when you only interested in the:
+  - End of wait time.
+  - Final State.
+* Examples:
+- If you don't want to fire that API call until the user has stopped typing in the search input. 
+*********************************************************************************************************************************************
+* Throttle is used when you are considering the:
+  - Intermediate States.
+  - At Intervals.
+* Examples:
+- When you're scrolling the page of a web application, and you want something to happen or update at a regular intervals as it scrolls.
+*/
+
 const initApp = () => {
   const tbutton = document.querySelector('#throttle');
   tbutton.addEventListener('click', throttle(clickLog, 2000));
