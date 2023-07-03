ea549083aee48a9b5bbad4694dc54ccf9adbf619
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-04 02:20:58 +0300

    Add getPostsSerialized function

--- stop_using_forEach.js
+++ stop_using_forEach.js
@@ -12,3 +12,11 @@ const getPost = async (id) => {
   return await (await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)).json();
 }
 
+const getPostsSerialized = async (ids) => {
+  for (const id of ids) {
+    const data = await getPost(id);
+    console.log(data);
+  }
+  console.log("I'll wait on you");
+}
+
