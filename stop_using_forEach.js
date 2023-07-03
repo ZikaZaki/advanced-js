d187fc6dce80454c75af7c8b174e41223be303c6
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-04 02:20:22 +0300

    Add initApp

--- stop_using_forEach.js
+++ stop_using_forEach.js
@@ -1,1 +1,9 @@
 
+const ids = [1,2,3,4,5,6,7,8,9,10];
+
+const initApp = async () => {
+  // getPostsSerialized(ids);
+  getPostsConcurrently(ids);
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
