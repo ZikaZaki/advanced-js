76df924bed18f258b857c10620a0f433d48bb0df
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-05 02:28:31 +0300

    Add initApp function

--- Memoization.js
+++ Memoization.js
@@ -1,1 +1,25 @@
 
+const initApp = async () => {
+//   const memoizedMultBy10 = memoize(multiplyBy10);
+//   console.log(memoizedMultBy10(10));
+//   console.log(memoizedMultBy10(10));
+//   console.log(memoizedMultBy10(5));
+//   console.log(memoizedMultBy10(5));
+  
+//   const memoizedAdd3 = memoize(add3);
+//   console.log(memoizedAdd3(1,2,3));
+//   console.log(memoizedAdd3(1,2,3));
+//   console.log(memoizedAdd3(4,5,6));
+//   console.log(memoizedAdd3(4,5,6));
+  
+//   const memoizedAddMany = memoize(addMany);
+//   console.log(memoizedAddMany(1,2,3,4,5,6));
+//   console.log(memoizedAddMany(1,2,3,4,5,6));
+//   console.log(memoizedAddMany(6,7,8,9,10));
+//   console.log(memoizedAddMany(6,7,8,9,10));
+  
+  const memoizedAddFib= memoize(fib);
+  console.log(memoizedAddFib(6));
+  console.log(memoizedAddFib(6));
+  console.log(memoizedAddFib(6));
+}
