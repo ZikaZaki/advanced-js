cf87d0b232e389accd74b2963d2b8032c2f1241e
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-07 00:36:22 +0300

    Create initApp function

--- stop_using_innerHTML.js
+++ stop_using_innerHTML.js
@@ -1,1 +1,8 @@
 
+const initApp = async () => {
+  const button1 = document.getElementById('1');
+  button1.addEventListener('click', createParas1);
+  
+  const button2 = document.getElementById('2');
+  button2.addEventListener('click', createParas2);
+}
