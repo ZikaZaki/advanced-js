e13098f86176597c3e41d0a87a39c8133cd684ca
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-07 00:46:03 +0300

    Add initApp function

--- stop_using_innerHTML2.js
+++ stop_using_innerHTML2.js
@@ -1,1 +1,8 @@
 
+const initApp = async () => {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    processUserInput();
+  });
+}
