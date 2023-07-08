1314306cad048b695f3ae9efd3500db4b916c33e
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-09 00:09:07 +0300

    Add log utility function

--- 12_useful_utility_functions.js
+++ 12_useful_utility_functions.js
@@ -3,3 +3,10 @@
 const properCase = (string) => {
   return `${string[0].toUpperCase()}${string.slice(1).toLowerCase()}`;
 };
+
+// #2 log: Console log any type of content.
+const log = (content) => {
+  console.log(content);
+};
+
+log(properCase("rEsEArcH"));
