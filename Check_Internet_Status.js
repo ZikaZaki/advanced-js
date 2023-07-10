c7ecfa3ee23eba23802edebf71b8f71ef8c72a6a
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-10 23:01:28 +0300

    Add checkOnlineStatus function

--- Check_Internet_Status.js
+++ Check_Internet_Status.js
@@ -15,3 +15,14 @@ window.addEventListener("online", (event) => {
   const statusDisplay = document.getElementById("status");
   statusDisplay.textContent = "Online";
 });
+/** *************************************************** */
+// Correct way: Request a small image at an interval to determine status
+const checkOnlineStatus = async () => {
+  try {
+    // const online = await fetch("/1pixel.png");
+    const online = await fetch("http://placekitten.com/g/2/3");
+    return online.status >= 200 && online.status < 300;
+  } catch (err) {
+    return false; // definitely offline
+  }
+}
