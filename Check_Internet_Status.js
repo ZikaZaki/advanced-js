66c8ef3b9f9e222ef3f3e700c3b3a58ab5ef50f8
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-10 22:59:56 +0300

    Add load event-listener

--- Check_Internet_Status.js
+++ Check_Internet_Status.js
@@ -1,1 +1,7 @@
 
+// The solution with navigator.online
+// Simple but has a problem...
+window.addEventListener("load", (event) => {
+  const statusDisplay = document.getElementById("status");
+  statusDisplay.textContent = navigator.online ? "Online" : "Offline";
+});
