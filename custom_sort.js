8edb4805e3f390a2a368eb9b1150e7a44f6e200f
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-11 23:53:56 +0300

    Create alphaSort function

--- custom_sort.js
+++ custom_sort.js
@@ -1,1 +1,13 @@
+/*======== Understanding the sort() method ========*/
 
+/*======== alpha sort order ========*/
+const alphaArray = ["zebra", "arial", "medium"];
+const alphaArray2 = ["zebra", "arial", "medium", 3, 1, 1000];
+
+const alphaSort = (array) => {
+  if (!array.length) return;
+  return array.sort();
+}
+
+console.log(alphaSort(alphaArray));
+console.log(alphaSort(alphaArray2));
