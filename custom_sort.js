6790afa66f1102ccae6aaca17bafc15e23429875
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-07-11 23:59:08 +0300

    Fix linter errors

--- custom_sort.js
+++ custom_sort.js
@@ -1,5 +1,4 @@
 /*======== Understanding the sort() method ========*/
-
 /*======== alpha sort order ========*/
 const alphaArray = ["zebra", "arial", "medium"];
 const alphaArray2 = ["zebra", "arial", "medium", 3, 1, 1000];
@@ -11,6 +10,7 @@ const alphaSort = (array) => {
 
 console.log(alphaSort(alphaArray));
 console.log(alphaSort(alphaArray2));
+
 /*======== Solution 1: Sort numbers in ascending order ========*/
 const arrayOfNumbers = [1, 1000, 21, 30, 4];
 const numberSortAsc = (array) => {
@@ -19,6 +19,7 @@ const numberSortAsc = (array) => {
 }
 
 console.log(numberSortAsc(arrayOfNumbers));
+
 /*======== Solution 2: Sort numbers in descending order ========*/
 // const arrayOfNumbers = [1, 10000, 21, 30, 4];
 const numberSortDesc = (array) => {
